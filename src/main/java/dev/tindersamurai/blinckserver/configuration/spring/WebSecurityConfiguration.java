d08f35f6a8c22d6e8bf47139cf27e3f54438d13d
HenryCo <henrycodev@gmail.com>
2019-04-29 17:04:46 +0200

    Deep rework Added: + JWT Support + Facebook authentication

--- src/main/java/dev/tindersamurai/blinckserver/configuration/spring/WebSecurityConfiguration.java
+++ src/main/java/dev/tindersamurai/blinckserver/configuration/spring/WebSecurityConfiguration.java
@@ -8,7 +8,6 @@ import dev.tindersamurai.blinckserver.security.filter.jwt.JWTLoginFilter;
 import dev.tindersamurai.blinckserver.security.token.service.TokenAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
